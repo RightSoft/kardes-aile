a48ba2ba79a1675eeb7cbdf8e4768ee07a9c6a41
Murat Nar <muratnar@rightsoft.co.uk>
2023-02-13 23:05:50 +0300

    Moderator module changes

--- Backend/src/KardesAile.CommonTypes/ViewModels/Moderators/SearchModeratorResult.cs
+++ Backend/src/KardesAile.CommonTypes/ViewModels/Moderators/SearchModeratorResult.cs
@@ -5,9 +5,9 @@ namespace KardesAile.CommonTypes.ViewModels.Moderators;
 public class SearchModeratorResult
 {
     public Guid Id { get; set; }
-    public string FirstName { get; set; } = string.Empty;
-    public string LastName { get; set; } = string.Empty;
-    public string Email { get; set; } = string.Empty;
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? Email { get; set; }
     public DateTime CreatedAt { get; set; }
     public UserStatuses Status { get; set; }
 }
